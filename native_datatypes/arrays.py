eab18e4d7dae8512fa778e84a0c9ca4e748f4832
daxinte <daxinte@gmail.com>
2018-04-25 16:24:21 +0300

    Arrays

---
+++ native_datatypes/arrays.py
@@ -0,0 +1,16 @@
+# It is the collection of elements of a single data type, eg. array of int, array of string.
+
+arr = [10, 20, 30, 40, 50]	
+print(arr[2])
+
+leng_arr = len(arr)
+print(leng_arr)
+
+arr.append(60)
+print(arr)
+
+# del arr(10)
+# print(arr)
+
+arr[-1] = '70'
+print(arr)
