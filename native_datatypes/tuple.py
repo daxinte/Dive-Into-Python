7ac0dac5903a526a834a5064600e88a9fcff840c
daxinte <daxinte@gmail.com>
2018-04-25 15:01:03 +0300

    Tuples

---
+++ native_datatypes/tuple.py
@@ -0,0 +1,8 @@
+first_tuple = (0,1,2,3,4,5,6,7,8)
+print(first_tuple)
+# print(first_tuple[6])
+# print(first_tuple[:1])
+print(first_tuple.index(6))
+print('x' in first_tuple)
+print(2 in first_tuple)
+
